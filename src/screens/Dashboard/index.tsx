07a8e112e0ec6f4b7a04a89a2d21a667bdcb3c14
Douglas Damasceno <douglasmdamasceno@gmail.com>
2021-09-17 19:33:22 -0300

    Header init creation

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Container ,Header} from './styles'
+import {
+    Container,
+    Header,
+    UserWrapper,
+    UserInfo,
+    Photo,
+    User,
+    UserGreeting,
+    UserName
+} from './styles'
 
 export default function Dashboard(){
     return (
         <Container>
-           <Header >
+            <Header >
+                <UserWrapper>
+               
+                <UserInfo>
+                    <Photo source={{uri:'https://avatars.githubusercontent.com/u/33847803?v=4'}}/>
+                    <User>
+                        <UserGreeting>Olá,</UserGreeting>
+                        <UserName>Douglas</UserName>
+                    </User>
+                </UserInfo>
+            </UserWrapper>
+
             </Header>
         </Container>
     )
