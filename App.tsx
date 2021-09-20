b833d7070c68e7bbca1cb2a7ad44e814db1bb56a
Douglas Damasceno <douglasmdamasceno@gmail.com>
2021-09-20 11:45:16 -0300

    navegation tab custumization

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
+import 'react-native-gesture-handler'
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components';
 
+import {NavigationContainer } from '@react-navigation/native';
+
 import theme from './src/global/styles/theme'
-import TelaDev from './src/screens/Register';
 
 import { useFonts,Poppins_400Regular,Poppins_500Medium,Poppins_700Bold} from '@expo-google-fonts/poppins'
 
+
+import{AppRoutes} from './src/routes/app.routes'
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -18,7 +22,9 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <TelaDev />
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
---
+++ src/routes/app.routes.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import {Feather} from '@expo/vector-icons'
+import { useTheme } from "styled-components";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Dashboard from '../screens/Dashboard';
+import Register from '../screens/Register';
+import { Platform } from "react-native";
+
+const {Navigator,Screen } = createBottomTabNavigator();
+
+export function AppRoutes() {
+    const theme = useTheme();
+    return (
+        <Navigator
+            screenOptions={{
+                headerShown: false,
+                tabBarActiveTintColor: theme.colors.secondary,
+                tabBarInactiveTintColor: theme.colors.primary,
+                tabBarLabelPosition:'beside-icon',
+                tabBarStyle: {
+                    paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+                    height: 72,
+                }
+            }}
+            
+        >
+            <Screen
+                name="Listagem"
+                component={Dashboard}
+                options={{
+                    tabBarIcon: (({ size, color }) => (
+                        <Feather
+                          name="list"  
+                            size={size}
+                            color={color}
+                        />
+                    ))
+                }}
+            />
+            <Screen
+                name="Cadastrar"
+                component={Register}
+                options={{
+                    tabBarIcon: (({ size, color }) => (
+                        <Feather
+                          name="dollar-sign"  
+                            size={size}
+                            color={color}
+                        />
+                    ))
+                }}
+            />
+            <Screen
+                name="Resumo"
+                component={Register}
+                options={{
+                    tabBarIcon: (({ size, color }) => (
+                        <Feather
+                          name="pie-chart"  
+                            size={size}
+                            color={color}
+                        />
+                    ))
+                }}
+            />
+        </Navigator>
+    )
+}
