8ac4324857024f08858248c2347048216ddb2a25
Douglas Damasceno <douglasmdamasceno@gmail.com>
2022-02-24 16:47:58 -0300

    criando tipo para usar svg e criacao de tela signIn

--- App.tsx
+++ App.tsx
@@ -14,6 +14,8 @@ import { useFonts,Poppins_400Regular,Poppins_500Medium,Poppins_700Bold} from '@e
 
 
 import{AppRoutes} from './src/routes/app.routes'
+import SignIn from './src/screens/SignIn';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_400Regular,
@@ -27,7 +29,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <NavigationContainer>
         <StatusBar barStyle='light-content' backgroundColor={theme.colors.primary}/>
-        <AppRoutes />
+        <SignIn />
       </NavigationContainer>
     </ThemeProvider>
   );
